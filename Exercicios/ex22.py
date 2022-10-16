3e417bf14e310be8a3f149f33b321b526b4fddbc
jeffersonkako <jefferson@kako.dev>
2022-10-16 13:58:40 -0300

    v1

---
+++ Exercicios/ex22.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print(nome)
+print(nome.upper())
+print(nome.lower())
+print(len(nome) - nome.count(' '))
+print()
+lista = nome.split()
+print(lista[0])
