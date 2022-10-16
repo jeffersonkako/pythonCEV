1620cfa55aa3f6a5d69fb4d0660bf499161e9e92
jeffersonkako <jefferson@kako.dev>
2022-10-16 10:26:21 -0300

    v1

--- Exercicios/ex79.py
+++ Exercicios/ex79.py
@@ -1,2 +1,16 @@
 ''' Exercício Python 079: Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.'''
 
+lista = []
+
+while True:
+    n = int(input('Digite um numero: '))
+    
+    if n not in lista:
+        lista.append(n)
+    else:
+        print('Valor duplicado.')
+        
+    r = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if r in 'Nn':
+        break
+lista.sort()
