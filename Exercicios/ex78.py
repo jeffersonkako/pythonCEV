51c7170e4de278ae02d50b3cce0f25c234d62bf7
jeffersonkako <jefferson@kako.dev>
2022-10-16 10:01:08 -0300

    v1

--- Exercicios/ex78.py
+++ Exercicios/ex78.py
@@ -1,8 +1,18 @@
 '''Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.'''
 
 lista1 = []
+men = 0
+maior = 0
 for v in range(0,10):
-    lista1.append(int(input('Digite um numero: ')))
+    lista1.append(int(input(f'Digite um numero para a posição {v}: ')))
+    if v == 0:
+        maior = men = lista1[v]
+    else:
+        if lista1[v] > maior:
+            maior = lista1[v]
+        if lista1[v] < men:
+            men = lista1[v]
+         
 print(f' O maior valor da lista foi {max(lista1)} e o menor valor lista foi {min(lista1)}')
 
 print(lista1)
