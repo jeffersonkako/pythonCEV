185c729422a3d55ce8c80ecf91c47249fd18ed73
jeffersonkako <jefferson@kako.dev>
2022-10-19 20:15:50 -0300

    v2

--- Exercicios/ex92.py
+++ Exercicios/ex92.py
@@ -1,4 +1,17 @@
 ''' Exercício Python 092: Crie um programa que leia nome, ano de nascimento e carteira de trabalho e 
 cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também 
 o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.'''
-
+from datetime import *
+dados = {}
+dados['Nome'] = str(input('Nome: '))
+nasc = int(input('Data de nascimento: '))
+dados['Idade'] = datetime.now().year - nasc
+dados['Ctps'] = int(input('Carteira de trabalho: [0 não tem] '))
+if dados['Ctps'] != 0:
+    dados['Contratação'] = int(input('Ano de contratação: '))
+    dados['Salario'] = int(input('Salario: '))
+    dados['Aposentadoria'] =dados['Idade'] + dados['Contratação'] + 35 - datetime.now().year
+    
+print('='* 30)
+for k, v in dados.items():
+    print(f'- {k}: {v} ')
